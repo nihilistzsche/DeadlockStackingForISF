125d94badb2fffd8d1d1f8544b0df149755c3eed
Nihilistzsche <nihilistzsche@gmail.com>
2023-01-22 23:03:34 -0800

    Fixes

--- migrations/items.lua
+++ migrations/items.lua
@@ -1,7 +1,7 @@
 local Items = {items = {}}
 
-for scienceName in pairs(data.raw.tool) do
-	if scienceNaem:sub(1, 8) == "infused-" then
+for scienceName in pairs(data and data.raw.tool or {}) do
+	if scienceName:sub(1, 8) == "infused-" then
 		Items.items[scienceName] = { type = "tool" }
 	end
 end
